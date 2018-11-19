00e730d3e29b417ab5ff8b3fde65516611199931
Dan Godiac <dan.godiac@strateg.se>
2018-11-19 15:25:32 +0100

    New title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel, hello from travis
+                    Laravel, hello from travis 2
                 </div>
 
                 <div class="links">
